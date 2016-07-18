675367b8c2f2190299e9eff14f3701c08d882282
Joshua Shi <thehandsomepanther@gmail.com>
2016-07-17 20:54:35 -0700

    cleaned up script

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function or(a, b) {
-  return a || b;
-};
-
-function removeChildren(node) {
-  while (node.firstChild) {
-    node.removeChild(node.firstChild);
-  }
-}
+var confetti_rect, W, H, window_center, PAD, screen_elements, COLS, ROWS, TOTAL_CONFETTI;
 
 var background = document.getElementById("background");
 var background_rect = background.getBoundingClientRect();
@@ -15,53 +7,73 @@ var confetti_1_clone = document.getElementById("confetti-1").cloneNode(true);
 var confetti_2_clone = document.getElementById("confetti-2").cloneNode(true);
 var confetti_3_clone = document.getElementById("confetti-3").cloneNode(true);
 var confetti_4_clone = document.getElementById("confetti-4").cloneNode(true);
-var confetti_clones = [confetti_1_clone, confetti_2_clone, confetti_3_clone, confetti_4_clone];
-var colors = ["green", "blue", "yellow", "purple", "orange", "red"];
-
-var w = window.innerWidth;
-var h = window.innerHeight;
-var window_center = {
-  x: Math.floor(w/2),
-  y: Math.floor(h/2)
-};
-
-var confetti_rect = getConfettiSize();
-
-var cols = Math.ceil(background_rect.width / confetti_rect.width);
-var rows = Math.ceil(background_rect.height / confetti_rect.height);
-background.style.width = cols * confetti_rect.width;
-
-var TOTAL_CONFETTI = cols * rows;
+var CONFETTI_CLONES = [confetti_1_clone, confetti_2_clone, confetti_3_clone, confetti_4_clone];
+var COLORS = ["green", "blue", "yellow", "purple", "orange", "red"];
 
 var RAD_TO_DEG = 180/Math.PI;
 
-var angle_noise = 25;
-var position_noise_x = 20;
-var position_noise_y = 20;
+var ANGLE_NOISE = 25;
+var POSITION_NOISE_X = 20;
+var POSITION_NOISE_Y = 20;
 
-var pad = .05 * h;
+setup();
+createBackground();
 
-var screen_elements = [
-  getCollisionArea(document.getElementById("presents").getBoundingClientRect(), pad),
-  getCollisionArea(document.getElementById("dummy").getBoundingClientRect(), pad),
-  getCollisionArea(document.getElementById("hackathon").getBoundingClientRect(), pad),
-  getCollisionArea(document.getElementById("mce-EMAIL").getBoundingClientRect(), pad),
-  getCollisionArea(document.getElementById("mc-embedded-subscribe").getBoundingClientRect(), pad)
-];
+function or(a, b) {
+  return a || b;
+};
 
-function getCollisionArea(box, pad) {
-  var collision_area = {};
+function randIntLessThan(b) {
+  return Math.floor((Math.random() * b));
+}
 
-  collision_area.top = box.top - pad;
-  collision_area.left = box.left - pad;
-  collision_area.height = box.height + (pad * 2);
-  collision_area.width = box.width + (pad * 2);
+function removeChildren(node) {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+}
+
+function setup() {
+  confetti_rect = getConfettiSize();
+
+  W = window.innerWidth;
+  H = window.innerHeight;
+  window_center = {
+    x: Math.floor(W/2),
+    y: Math.floor(H/2)
+  };
+
+  PAD = .05 * H;
+
+  screen_elements = [
+    getCollisionArea(document.getElementById("presents").getBoundingClientRect(), PAD),
+    getCollisionArea(document.getElementById("dummy").getBoundingClientRect(), PAD),
+    getCollisionArea(document.getElementById("hackathon").getBoundingClientRect(), PAD),
+    getCollisionArea(document.getElementById("mce-EMAIL").getBoundingClientRect(), PAD),
+    getCollisionArea(document.getElementById("mc-embedded-subscribe").getBoundingClientRect(), PAD),
+  ];
+
+  confetti_rect = getConfettiSize();
+  COLS = Math.ceil(background_rect.width / confetti_rect.width);
+  ROWS = Math.ceil(background_rect.height / confetti_rect.height);
+  background.style.width = COLS * confetti_rect.width;
+
+  TOTAL_CONFETTI = COLS * ROWS;
+}
+
+function getCollisionArea(box, PAD) {
+  var collision_area = {
+    top: box.top - PAD,
+    left: box.left - PAD,
+    height: box.height + (PAD * 2),
+    width: box.width + (PAD * 2)
+  };
 
   return collision_area;
 }
 
 function getConfettiSize() {
-  var dummy = confetti_clones[0].cloneNode(true);
+  var dummy = CONFETTI_CLONES[0].cloneNode(true);
   dummy.id = "";
   background.appendChild(dummy);
   var dummy_rect = dummy.getBoundingClientRect();
@@ -69,51 +81,18 @@ function getConfettiSize() {
   return dummy_rect;
 }
 
-function clearBackground() {
-  removeChildren(background)
-}
-
 function resetBackground(center) {
-  clearBackground();
-
-  confetti_rect = getConfettiSize();
-
-  w = window.innerWidth;
-  h = window.innerHeight;
-  window_center = {
-    x: Math.floor(w/2),
-    y: Math.floor(h/2)
-  };
-
-  screen_elements = [
-    getCollisionArea(document.getElementById("presents").getBoundingClientRect(), pad),
-    getCollisionArea(document.getElementById("dummy").getBoundingClientRect(), pad),
-    getCollisionArea(document.getElementById("hackathon").getBoundingClientRect(), pad),
-    getCollisionArea(document.getElementById("mce-EMAIL").getBoundingClientRect(), pad),
-    getCollisionArea(document.getElementById("mc-embedded-subscribe").getBoundingClientRect(), pad),
-  ];
-
-  confetti_rect = getConfettiSize();
-  cols = Math.ceil(background_rect.width / confetti_rect.width);
-  rows = Math.ceil(background_rect.height / confetti_rect.height);
-  background.style.width = cols * confetti_rect.width;
-
-  TOTAL_CONFETTI = cols * rows;
-
+  removeChildren(background);
+  setup();
   createBackground();
 }
 
 function createBackground() {
-  // insert here a way to clear the entire background
-
   for (var i = 0; i < TOTAL_CONFETTI; i++){
-    var rand_confetti = Math.floor(Math.random() * 10) % confetti_clones.length;
-
-    var new_confetti = confetti_clones[rand_confetti].cloneNode(true);
+    var new_confetti = CONFETTI_CLONES[randIntLessThan(CONFETTI_CLONES.length)].cloneNode(true);
     new_confetti.id = "new-confetti";
 
-    var rand_color = Math.floor(Math.random() * 10) % colors.length;
-    new_confetti.className += " " + colors[rand_color];
+    new_confetti.className += " " + COLORS[randIntLessThan(COLORS.length)];
 
     background.appendChild(new_confetti);
 
@@ -144,41 +123,42 @@ function createBackground() {
         y: window_center.y - rect_center.y
       }
 
-      var position_adjustment = Math.floor(Math.random() * 100) % position_noise_x - position_noise_x;
+      var position_adjustment = Math.floor(Math.random() * 100) % POSITION_NOISE_X - POSITION_NOISE_X;
 
       new_confetti.style.marginLeft = 0 + "px";
-      position_adjustment = Math.floor(Math.random() * 100) % position_noise_y - position_noise_y;
+      position_adjustment = Math.floor(Math.random() * 100) % POSITION_NOISE_Y - POSITION_NOISE_Y;
       new_confetti.style.marginTop = position_adjustment + "px";
 
       var angle_adjustment = 0;
       var angle = Math.atan2(vector.y, vector.x) * RAD_TO_DEG + 270 + angle_adjustment;
 
-      // var angle_adjustment = Math.floor(Math.random() * 100) % angle_noise - angle_noise;
+      var angle_adjustment = Math.floor(Math.random() * 100) % ANGLE_NOISE - ANGLE_NOISE;
+      angle += angle_adjustment
       new_confetti.style.transform = "rotate(" + angle + "deg)";
     }
 
-    new_confetti.removeAttribute("id")
+    new_confetti.removeAttribute("id");
   }
 };
 
 var confettis = document.getElementsByClassName("svg-wrap");
 var up = "first";
-var theta = 10;
+var THETA = 10;
 
 function wiggle() {
   for (i = 0; i < confettis.length; i++) {
     var angle = parseFloat(confettis[i].style.transform.replace("rotate(", "").replace("deg)", ""));
-    angle += (up ? theta : -theta)
+    angle += (up ? THETA : -THETA)
     confettis[i].style.transform = "rotate(" + angle + "deg)";
   }
-  if (typeof up == "string") theta *= 2;
+  if (typeof up == "string") THETA *= 2;
   up = !up;
 }
 
 function randomWiggle() {
   for (i = 0; i < confettis.length; i++) {
     var angle = parseFloat(confettis[i].style.transform.replace("rotate(", "").replace("deg)", ""));
-    angle += (Math.ceil(Math.random() - .5) ? theta : -theta)
+    angle += (Math.ceil(Math.random() - .5) ? THETA : -THETA)
     confettis[i].style.transform = "rotate(" + angle + "deg)";
   }
 }
@@ -200,15 +180,14 @@ function track(center) {
 }
 
 window.addEventListener("resize", function() {
-  resetBackground()
+  resetBackground();
 });
 
 var mouse_pos = window_center;
 var new_mouse_pos = mouse_pos;
 
-createBackground(window_center);
+switch(randIntLessThan(5)) {
 
-switch(Math.floor((Math.random() * 10) % 5)) {
   case 4:
     window.addEventListener("mousemove", function(e) {
       new_mouse_pos = {
@@ -233,10 +212,10 @@ switch(Math.floor((Math.random() * 10) % 5)) {
     break;
 
   case 1:
-    setInterval(wiggle, 500)
+    setInterval(wiggle, 500);
     break;
 
   default:
-    setInterval(randomWiggle, 500)
+    setInterval(randomWiggle, 500);
 }
 
